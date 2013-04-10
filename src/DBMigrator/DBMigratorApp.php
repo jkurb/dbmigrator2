cf5f5b03f036a0f39f7c775ba2c836e44d5d4b2f
Eugene Kurbatov <ekur@i-loto.ru>
2013-04-10 16:57:03 +0400

    removed composer dir

--- src/DBMigrator/DBMigratorApp.php
+++ src/DBMigrator/DBMigratorApp.php
@@ -5,33 +5,33 @@ namespace DBMigrator;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Yaml\Yaml;
 
-class DBMigratorApp extends Application 
-{		
+class DBMigratorApp extends Application
+{
 	const DEFAULT_CONFIG_NAME = "dbmigrator.yml";
 
 	/**
 	 * Конфиг
-	 * 
+	 *
 	 * @var array
 	 */
 	public $config = null;
-	
-    public function __construct() 
+
+    public function __construct()
     {
     	parent::__construct("Welcome to DBMigrator", "2.0");
-	    $this->setCatchExceptions(true);	 
+	    $this->setCatchExceptions(true);
 	    $this->setAutoExit(false);
 
         // todo: load commands from dir
     	$this->addCommands(array(
-			new Command\Check(),			
-			new Command\Init(),			
-			new Command\Commit(),			
-			new Command\Create(),			
-			new Command\Diff(),			
-			new Command\Log(),			
-			new Command\Migrate(),			
-			new Command\Status()							
+			new Command\Check(),
+			new Command\Init(),
+			new Command\Commit(),
+			new Command\Create(),
+			new Command\Diff(),
+			new Command\Log(),
+			new Command\Migrate(),
+			new Command\Status()
 		));
     }
 
