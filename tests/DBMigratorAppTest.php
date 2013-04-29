458cec7f07a7ffcf91b2a3b9f2f3eeec52bfb332
Eugene Kurbatov <ekur@i-loto.ru>
2013-04-29 17:34:46 +0400

    изменен тест

--- tests/DBMigratorAppTest.php
+++ tests/DBMigratorAppTest.php
@@ -6,16 +6,16 @@ use Symfony\Component\Console\Tester\ApplicationTester;
 
 class DBMigratorAppTest extends PHPUnit_Framework_TestCase
 {
-	public function testRun() 
-	{		
+	public function testRun()
+	{
 		$migrator = new DBMigrator\DBMigratorApp();
 		$migrator->setAutoExit(false);
 		$migratorTester = new ApplicationTester($migrator);
 
 		$migratorTester->run(array());
-		
-		$disp = $migratorTester->getDisplay();		
+
+		$disp = $migratorTester->getDisplay();
 		$this->assertNotRegExp("/Exception/", $disp);
-		$this->assertRegExp("/^Welcome to DBMigrator version 2.0/", $disp);
+		$this->assertRegExp("/Welcome to DBMigrator version 2.0/", $disp);
 	}
 }
